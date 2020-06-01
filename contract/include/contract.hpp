92939eaa0dd75953e57cb0f6dd2c63ada0bce90a
Mason Brothers <brothers.mason@gmail.com>
2020-06-01 00:08:30 -0600

    It builds now

--- contract/include/contract.hpp
+++ contract/include/contract.hpp
@@ -3,79 +3,107 @@
 #include <eosio/asset.hpp>
 #include <eosio/eosio.hpp>
 
+#include <eosio/system.hpp>
+
 #include <string>
+
+#define SECONDS_VOTING_LASTS 60
 
 namespace eosiosystem {
    class system_contract;
 }
 
 namespace eosio {
-
    using std::string;
 
-   class [[eosio::contract("eosio.token")]] token : public contract {
+   class [[eosio::contract("factfinder")]] factfinder : public contract {
+      private:
+      const symbol eos_symbol;
+
       public:
          using contract::contract;
          
+         factfinder(name receiver, name code, datastream<const char *> ds) : 
+            contract(receiver, code, ds),
+            eos_symbol("SYS", 4){
+
+               index_table indicies( get_self(), get_first_receiver().value );
+               indicies.emplace(get_self(), [&]( auto& s ) {
+                  s.vote_index = 0;
+                  s.message_index = 0;
+               });
+            }
+
          [[eosio::action]]
-         void post( const name& poster, const std::string& message, const asset& amount, const int64_t& reply_id ) {
+         void post( const name& poster, const std::string& message ) {
             require_auth( poster );
 
             check( is_account( poster ), "poster account does not exist" );
 
-            auto sym_code_raw = symbol.code().raw();
-            stats statstable( get_self(), sym_code_raw );
-            const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
-            check( st.supply.symbol == symbol, "symbol precision mismatch" );
+            index_table indicies( get_self(), get_first_receiver().value );
+            index i = indicies.get(0);
+            // TODO NEED TO UPDATE INDEX
 
-            accounts acnts( get_self(), owner.value );
-            auto it = acnts.find( sym_code_raw );
-            if( it == acnts.end() ) {
-               acnts.emplace( poster, [&]( auto& a ){
-               a.balance = asset{0, symbol};
-               });
-            }
+            message_table messages( get_self(), get_first_receiver().value );
 
-            action{
-               permission_level{get_self(), "active"_n},
-               get_self(),
-               "vote"_n,
-               std::make_tuple(poster, message_id, amount, true)
-            }.send();
+            time_point current_time = current_time_point();
+
+            messages.emplace( poster, [&]( auto& s ) {
+               s.poster = poster;
+               s.message = message;
+               s.message_id    = i.message_index + 1;
+               s.post_time     = current_time;
+               s.staked_true   = 0;
+               s.staked_false  = 0;
+            });
+
          }
 
          [[eosio::action]]
-         void vote( const name& voter, const asset& amount, const bool& is_true ) {
+         void castvote( const name& voter, const uint64_t& message_id, const asset& amount, const bool& is_true ) {
             require_auth( voter );
             check( is_account( voter ), "voter account does not exist" );
 
-            accounts stakes( get_self(),  );
-            auto it = acnts.find( sym_code_raw );
-            if( it == acnts.end() ) {
-               acnts.emplace( poster, [&]( auto& a ){
-               a.balance = asset{0, symbol};
-               });
+            
+
+            /*
+            message_table messages;
+            auto message = messages.find(message_id);
+
+            if (message == messages.end()) {
+               check(false, "message does not exist");
             }
 
+            check(isActive(message), "voting is over");
+
+            */
+            /*
             action{
                permission_level{get_self(), "active"_n},
                "eosio.token"_n,
                "transfer"_n,
                std::make_tuple(owner, get_self() hodl_it->funds, std::string("Staking Tokens for Voting"))
             }.send();
+            */
          };
 
          [[eosio::action]]
-         void solidify( const uint64_t& id ) {
+         void withdraw( const uint64_t& vote_id ) {
+            
 
-            check(now() > the_party, "Hold your horses");
+            /*
+            action{
+               permission_level{get_self(), "active"_n},
+               "eosio.token"_n,
+               "transfer"_n,
+               std::make_tuple(get_self(),  hodl_it->funds, std::string("Staking Tokens for Voting"))
+            }.send();
+            */
          }
 
-         void withdraw( const uint64_t& vote_id )
-
-         using post_action = eosio::action_wrapper<"post"_n, &token::post>;
-         using vote_action = eosio::action_wrapper<"vote"_n, &token::vote>;
-         using solidify_action = eosio::action_wrapper<"solidify"_n, &token::solidify>;
+         using post_action = eosio::action_wrapper<"post"_n, &factfinder::post>;
+         using castvote_action = eosio::action_wrapper<"castvote"_n, &factfinder::castvote>;
+         using withdraw_action = eosio::action_wrapper<"withdraw"_n, &factfinder::withdraw>;
       private:
          struct [[eosio::table]] vote {
             asset    balance;
@@ -86,21 +114,31 @@ namespace eosio {
          };
 
          struct [[eosio::table]] message {
-            name     poster;
+            name        poster;
             std::string message;
-            uint64_t message_id;
-            uint64_t reply_to;
-            uint32_t post_time;
-            uint64_t staked_true;
-            uint64_t staked_false;
-
-
+            uint64_t    message_id;
+            time_point  post_time;
+            uint64_t    staked_true;
+            uint64_t    staked_false;
 
             uint64_t primary_key()const { return message_id; }
          };
 
-         typedef eosio::multi_index< "messages"_n, account > messages;
-         typedef eosio::multi_index< "stakes"_n, stake > stakes;
+         struct [[eosio::table]] index {
+            uint64_t vote_index;
+            uint64_t message_index;
+
+            uint64_t primary_key()const { return 0; }
+         };
+
+         typedef eosio::multi_index< "messages"_n, message > message_table;
+         typedef eosio::multi_index< "votes"_n, vote > vote_table;
+         typedef eosio::multi_index< "indecies"_n, index > index_table;
+
+
+         bool is_active(const message& msg) {
+            return (msg.post_time + time_point(seconds(SECONDS_VOTING_LASTS))) < eosio::current_time_point();
+         }
    };
 
 }
